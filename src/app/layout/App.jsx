3a88434f3dd4d558a99f7e4539e661379df7060f
Ricardo James <ricardo.james@plex-llc.com>
2018-07-25 16:15:30 -0400

    Added axios

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import axios from 'axios';
 import EventDashboard from '../../features/Event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar'
-
 
 class App extends Component {
   state = {
--- src/features/Event/EventDashboard/EventDashboard.jsx
+++ src/features/Event/EventDashboard/EventDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
-import EventSingle from '../../features/Event/EventDashboard/EventSingle/EventSingle'
+import axios from 'axios';
+import EventSingle from '../EventDashboard/EventSingle'
 
 class EventDashboard extends Component {
     state = {
@@ -9,6 +10,14 @@ class EventDashboard extends Component {
     
     componentDidMount(){
         const url = `https://www.eventbriteapi.com/v3/events/search?token=IZQ47CP6HI5KKV7V5ADC`;
+
+        axios.get(url)
+         .then((res) => {
+             this.setState({
+                 event: res.data.events
+             })
+         })
+         .catch((error) => console.log(error));
     }
 
     renderItems(){
